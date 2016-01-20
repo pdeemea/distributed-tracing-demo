2e9d9cc35e1c3615dcc86ffa9338a15af36f4095
Marcial Rosales <mrosales@pivotal.io>
2016-01-20 12:30:32 +0100

    Get AsyncController working

--- gateway/src/main/java/io/pivotal/demo/gateway/AsyncController.java
+++ gateway/src/main/java/io/pivotal/demo/gateway/AsyncController.java
@@ -1,6 +1,6 @@
 package io.pivotal.demo.gateway;
 
-import java.util.Date;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
@@ -18,59 +18,110 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import io.pivotal.demo.gateway.SynchronousController.DealDone;
+import io.pivotal.demo.gateway.SynchronousController.Position;
+import io.pivotal.demo.gateway.SynchronousController.Trade;
+import io.pivotal.demo.gateway.SynchronousController.TradeRequest;
 
 @RestController
 @RequestMapping("/async")
-public class AsyncController  {
+public class AsyncController {
 
 	private static Logger log = LoggerFactory.getLogger(AsyncController.class);
-	
+
 	@Autowired
 	private TraceManager traceManager;
-	
+
 	@Autowired
 	private AsyncRestTemplate asyncRestTemplate;
 
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Value("${marketgw}") 
+	@Value("${marketgw}")
 	private String marketgw;
-	
-	@Value("${portfoliomgr}") 
+
+	@Value("${portfoliomgr}")
 	private String portfoliomgr;
-	
-	
+
 	@RequestMapping(value = "open", method = RequestMethod.POST)
-	public ListenableFuture<ResponseEntity<SynchronousController.Position>> open(@RequestBody SynchronousController.TradeRequest request) throws InterruptedException, ExecutionException {
+	public DeferredResult<SynchronousController.Position> open(
+			@RequestBody SynchronousController.TradeRequest request) throws InterruptedException, ExecutionException {
 		Span span = traceManager.getCurrentSpan();
 
 		log.info("Opening trade {} {} @ {}", request.account, request.amount, span.getTraceId());
-		
-		span.addAnnotation("account", request.account);
-		
-		// First async rest call: Execute the trade by calling market Gateway Restful service.  
-		ListenableFuture<ResponseEntity<SynchronousController.Trade>> openedMktTrade = this.asyncRestTemplate.postForEntity(
-				marketgw + "/openTrade", 
-				new HttpEntity<SynchronousController.MktTradeRequest>(new SynchronousController.MktTradeRequest(request)),
-				SynchronousController.Trade.class);
 
-//		openedMktTrade.addCallback(t -> {
-//			
-//			calculateSomeOtherComplexValue(t.getBody());
-//			
-//			restTemplate.postForObject(portfoliomgr + "/openPosition", trade, Trade.class);
-//			
-//			
-//		}, e -> { } );
-		// TODO when we receive an openedMktTrade we call calculateSomeOehterComplexValue and then we asynchronously call
-		// the Restful service positionMgr.
+		span.addAnnotation("account", request.account);
+
+		DeferredResult<SynchronousController.Position> deferredResult = new DeferredResult<>();
+
+		// First async rest call: Execute the trade by calling market Gateway
+		// Restful service.
+		ListenableFuture<ResponseEntity<SynchronousController.Trade>> openedMktTrade = this.asyncRestTemplate
+				.postForEntity(marketgw + "/openTrade", new HttpEntity<SynchronousController.MktTradeRequest>(
+						new SynchronousController.MktTradeRequest(request)), SynchronousController.Trade.class);
+
+		openedMktTrade.addCallback((ResponseEntity<SynchronousController.Trade> t) -> {
+			
+			DealDone deal = applySpread(request, t.getBody());
+			
+			Position position = restTemplate.postForObject(portfoliomgr + "/openPosition", deal, Position.class);
+			
+            deferredResult.setResult(position);
+                
+		}, e -> {
+			deferredResult.setErrorResult(e);
+		}
+        );
 		
-		return null; 		// TODO return the ListenableFuture from the 2nd async rest call
+		return deferredResult; 
+
+	}
+    @RequestMapping(value = "/deferred", method = RequestMethod.GET, produces = "text/html")
+    public DeferredResult<String> executeSlowTask() {
+        log.info("Request received");
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        CompletableFuture.supplyAsync(AsyncController::execute)
+            .whenCompleteAsync((result, throwable) -> deferredResult.setResult(result));
+        log.info("Servlet thread released");
+        
+        return deferredResult;
+    }
+
+    private static String execute() {
+    	try {
+			Thread.sleep(2000);
+			return "hello";
+		} catch (InterruptedException e) {
+			return "interrupted";
+		}
+    }
+    
+	/**
+	 * Calculate other complex value. Track the time spent.  
+	 * @throws InterruptedException 
+	 */
+	public DealDone applySpread(TradeRequest request, Trade trade) {
+	
+		Span span = traceManager.getCurrentSpan();
+		
+		// do something
+		try {
+			Thread.sleep(250);
+			double price = trade.rate + 0.01; // very simplistic spread. 
+			return new DealDone(trade.id, trade.symbol, request.account, price, trade.amount);
+		}catch(InterruptedException e) {
+			return null;
+		}finally {
+			if (span !=  null) {
+				span.addTimelineAnnotation("AppliedSpread");
+			}else {
+				log.warn("No Span avaliable in applySpread method");
+			}
+		}
 		
 	}
-	
 
-	
 }
-
--- gateway/src/main/java/io/pivotal/demo/gateway/GatewayConfiguration.java
+++ gateway/src/main/java/io/pivotal/demo/gateway/GatewayConfiguration.java
@@ -3,6 +3,7 @@ package io.pivotal.demo.gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.AsyncRestTemplate;
 
 @Configuration
